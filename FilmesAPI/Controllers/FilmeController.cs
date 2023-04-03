bf91ae796763a8b47aeb308580eba41bb937ff19
Ton DeVeros <TonDeveros@gmail.com>
2023-04-02 22:06:47 -0300

    Conexão ao banco de dados; Adição das classes Dto para deixar o modelo das tabelas menos expostos; Adição do método de Update E Delete; Adição de documentação do Swagger;

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -1,6 +1,12 @@
-﻿using FilmesAPI.Models;
+﻿using AutoMapper;
+using FilmesAPI.Data;
+using FilmesAPI.Data.Dtos;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Collections.Generic;
+using System.Text.Json.Serialization.Metadata;
 
 namespace FilmesAPI.Controllers
 {
@@ -9,34 +15,92 @@ namespace FilmesAPI.Controllers
     [Route("[controller]")]
     public class FilmeController : ControllerBase
     {
-        //instanciando uma variavel do tipo listaDeFilmes para fazer a lista em memória
-        private static List<Filme> filmes = new List<Filme>();
-        private static int idFilme = 0;
+        private FilmeContext _context;
+        private IMapper _mapper;
 
-        [HttpPost]
-        public IActionResult AdcionarFilme([FromBody] Filme filme)
+        public FilmeController(FilmeContext context, IMapper mapper)
         {
-            filme.Id = idFilme++;
-            filmes.Add(filme);//adcionando um filme a listaDeFilmes
+            _context = context;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Adiciona um filme ao banco de dados
+        /// </summary>
+        /// <param name="filmeDto">Objeto com os campos necessários para criação de um filme</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="201">Caso inserção seja feita com sucesso</response>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public IActionResult AdcionarFilme([FromBody] CreateFilmeDto filmeDto)
+        {
+            Filme filme = _mapper.Map<Filme>(filmeDto);
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
             return CreatedAtAction(nameof(SelecionarFilmePorId), new { id = filme.Id }, filme);
         }
 
         [HttpGet]
-        public IEnumerable<Filme> ListarFilmes( //posso retornar uma List<Filme> mas um IEnumerable é mais garantido que não precise mudar o código posteriormente
+        public IEnumerable<ReadFilmeDto> ListarFilmes( //posso retornar uma List<Filme> mas um IEnumerable é mais garantido que não precise mudar o código posteriormente
             [FromQuery] int skip = 0    //valor padrao do skip será 0,
             ,[FromQuery] int take = 50 // valor padrao do take será 50
             ) 
         {
-            return filmes.Skip(skip).Take(take);
+            return _mapper.Map <List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take));
         }
 
         [HttpGet("{id}")]
         //public Filme? SelecionarFilmePorId(int id)// Aqui seria o retorno do próprio objeto de filme
         public IActionResult SelecionarFilmePorId(int id)// o retorno de IActionResult é o retorno de uma ação 
         {
-            var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
             if (filme == null) return NotFound();
-            return Ok(filme);
+            var filmeDto = _mapper.Map<ReadFilmeDto>(filme);
+            return Ok(filmeDto);
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizaFilme(int id, [FromBody] UpdateFilmeDto filmeDto)
+        {
+            var filme = _context.Filmes.FirstOrDefault(
+                filme => filme.Id == id);
+            if (filme == null) return NotFound();
+            _mapper.Map(filmeDto, filme);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult AtualizaFilmeParcial(int id, JsonPatchDocument<UpdateFilmeDto> patch)
+        {
+            var filme = _context.Filmes.FirstOrDefault(
+               filme => filme.Id == id);
+            if (filme == null) return NotFound();
+
+            var filmeParaAtualizar = _mapper.Map<UpdateFilmeDto>(filme);
+
+            patch.ApplyTo(filmeParaAtualizar, ModelState);//validacao para ver se o filme é valido
+
+            if(!TryValidateModel(filmeParaAtualizar))
+            {
+                return ValidationProblem(ModelState);
+            }
+            _mapper.Map(filmeParaAtualizar, filme);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletaFilme(int id)
+        {
+            var filme = _context.Filmes.FirstOrDefault(
+               filme => filme.Id == id);
+            if (filme == null) return NotFound();
+
+            _context.Remove(filme);
+            _context.SaveChanges();
+            return NoContent();
         }
 
     }
---
+++ FilmesAPI/Data/Dtos/UpdateFilmeDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class UpdateFilmeDto
+    {
+        [Required(ErrorMessage = "O Titulo do filme é obrigatório!")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O Genero do filme é obrigatório!")]
+        [StringLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres!")]
+        public string Genero { get; set; }
+        [Required]
+        [Range(70, 600, ErrorMessage = "A duração do filme deve ser entre 70 e 600 minutos!")]
+        public int Duracao { get; set; }
+    }
+}
